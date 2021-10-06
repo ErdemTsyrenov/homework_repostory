b3df8b55760531502c512bf6cbc005967b7dfd56
Erdem <46107862+ErdemTsyrenov@users.noreply.github.com>
2021-10-06 14:49:19 +0300

    dir in tests fixed (#9)

--- tests/homework1/test_task3.py
+++ tests/homework1/test_task3.py
@@ -4,32 +4,39 @@ import pytest
 
 from homework1.task3 import find_maximum_and_minimum
 
-os.chdir('tests/homework1')
+
+@pytest.fixture()
+def chdir():
+    cwd = os.getcwd()
+    print(cwd)
+    os.chdir('tests/homework1')
+    yield
+    os.chdir(cwd)
 
 
 @pytest.mark.parametrize('test_input, expected',
                          [('test_task3_files/test1.txt', (8, 1))]
                          )
-def test_ascending_numbers(test_input, expected):
+def test_ascending_numbers(chdir, test_input, expected):
     assert find_maximum_and_minimum(test_input) == expected
 
 
 @pytest.mark.parametrize('test_input, expected',
                          [('test_task3_files/test2.txt', (8, 1))]
                          )
-def test_descending_number(test_input, expected):
+def test_descending_number(chdir, test_input, expected):
     assert find_maximum_and_minimum(test_input) == expected
 
 
 @pytest.mark.parametrize('test_input, expected',
                          [('test_task3_files/test3.txt', (55, -8))]
                          )
-def test_with_negative_numbers(test_input, expected):
+def test_with_negative_numbers(chdir, test_input, expected):
     assert find_maximum_and_minimum(test_input) == expected
 
 
 @pytest.mark.parametrize('test_input, expected',
                          [('test_task3_files/test4.txt', (4, 4))]
                          )
-def test_equal_numbers(test_input, expected):
+def test_equal_numbers(chdir, test_input, expected):
     assert find_maximum_and_minimum(test_input) == expected
--- tests/homework2/test_task1.py
+++ tests/homework2/test_task1.py
@@ -1,8 +1,18 @@
 import os
 
+import pytest
+
 from homework2.task1 import get_longest_diverse_words
 
 
-def test_get_longest_diverse_words():
+@pytest.fixture()
+def chdir():
+    cwd = os.getcwd()
+    print(cwd)
     os.chdir('tests/homework2')
+    yield
+    os.chdir(cwd)
+
+
+def test_get_longest_diverse_words(chdir):
     get_longest_diverse_words('data.txt')
